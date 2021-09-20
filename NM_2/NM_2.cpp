e590bf1a673dd08a270ea8029bb34b7f8b720b69
L1mmp <dima.wwe2@yandex.ru>
2021-09-20 19:22:56 +0300

    Добавьте файлы проекта.

---
+++ NM_2/NM_2.cpp
@@ -0,0 +1,53 @@
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <math.h>
+
+double TrapezeMethod(double a, double b, double e, double n);
+double F(double x);
+
+using namespace std;
+
+
+int main()
+{
+	cout.precision(6);
+	double epsilone = 10e-5;
+	double maximumDif2F = 0.179587;
+	cout << "Integral F(x) = " << TrapezeMethod(0, M_PI, epsilone, 3000);
+}
+
+double F(double x)
+{
+	if (x == 0) return 0.0;
+	return sin(x) / sqrt(x);
+}
+
+
+
+double TrapezeMethod(double a, double b, double e, double n)
+{
+	double h = (b - a) / n;
+
+	//cout << "h = " << h << endl;
+
+	double s = h / 2 * (F(a) + F(b));
+	double r = -n * pow(h, 3) / 12 * 0.5;
+
+	//cout << "s0 = " << s << '\n';
+
+	for (int i = 1; i < n; i++)
+	{
+		s += (F(a + h * i) * h);
+		//cout << "s" << i << " = " << s << '\n';
+	}
+
+	cout << "R = " << r << endl;
+
+	return s;
+}
+
+double GaussMethod(double a, double b, double e, double n)
+{
+
+}
