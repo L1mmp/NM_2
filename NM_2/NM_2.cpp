8f4f670e2fc9f831729e9a58dce5e16679092680
L1mmp <dima.wwe2@yandex.ru>
2021-09-27 23:22:44 +0300

    qwe

--- NM_2/NM_2.cpp
+++ NM_2/NM_2.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <math.h>
 
-double TrapezeMethod(double a, double b, double e, double n);
+double TrapezeMethod(double a, double b, double n);
+double GaussMethod(double a, double b, double n);
 double F(double x);
+double GaussF(double t, double a, double b);
 
 using namespace std;
 
@@ -12,42 +14,53 @@ using namespace std;
 int main()
 {
 	cout.precision(6);
-	double epsilone = 10e-5;
-	double maximumDif2F = 0.179587;
-	cout << "Integral F(x) = " << TrapezeMethod(0, M_PI, epsilone, 3000);
+
+	const double epsilone = 10e-5;
+	const double maximumDif2F = 0.179587;
+
+	cout << "Integral F(x) = " << TrapezeMethod(0, M_PI, 2800) << '\n';
+	cout << "Integral F(x) = " << GaussMethod(0, M_PI, 4) << '\n';
 }
 
 double F(double x)
 {
-	if (x == 0) return 0.0;
+	if (x == 0) return 0;
 	return sin(x) / sqrt(x);
+}
+
+double GaussF(double t, double a, double b)
+{
+	cout << (b - a / 2) * F((a + b / 2) + (b - a / 2) * t) << endl;
+	return (b - a / 2) * F((a + b / 2) + (b - a / 2) * t);
 }
 
 
 
-double TrapezeMethod(double a, double b, double e, double n)
+double TrapezeMethod(double a, double b, double n)
 {
 	double h = (b - a) / n;
 
-	//cout << "h = " << h << endl;
-
 	double s = h / 2 * (F(a) + F(b));
-	double r = -n * pow(h, 3) / 12 * 0.5;
-
-	//cout << "s0 = " << s << '\n';
 
 	for (int i = 1; i < n; i++)
 	{
 		s += (F(a + h * i) * h);
-		//cout << "s" << i << " = " << s << '\n';
 	}
-
-	cout << "R = " << r << endl;
 
 	return s;
 }
 
-double GaussMethod(double a, double b, double e, double n)
+double GaussMethod(double a, double b, double n)
 {
+	double s = 0;
 
+	double t[3] = { -0.774596669, 0.0, 0.774596669 };
+	double A[3] = { 0.555555556, 0.888888889, 0.555555556 };
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		s += A[i] * GaussF(t[i], a, b);
+	}
+
+	return s;
 }
